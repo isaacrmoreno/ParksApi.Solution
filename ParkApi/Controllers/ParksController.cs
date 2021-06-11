3ba8c534f6cc2e222511a65c3c7057e082a4629c
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-11 14:24:56 -0700

    update

--- ParkApi/Controllers/ParksController.cs
+++ ParkApi/Controllers/ParksController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ParkApi.Models;
@@ -24,15 +27,6 @@ namespace ParkApi.Controllers
       return await _db.Parks.ToListAsync();
     }
 
-    // POST api/parks
-    [HttpPost]
-    public async Task<ActionResult<Park>> Post(Park park)
-    {
-      _db.Parks.Add(park);
-      await _db.SaveChangesAsync();
-      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
-
-    }
     // GET: api/Parks/5
     [HttpGet("{id}")]
     public async Task<ActionResult<Park>> GetPark(int id)
@@ -45,6 +39,49 @@ namespace ParkApi.Controllers
         }
 
         return park;
+    }
+
+        // PUT: api/Parks/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(park).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+    
+    // POST api/parks
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
+    }
+      private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(e => e.ParkId == id);
     }
   } 
 }
