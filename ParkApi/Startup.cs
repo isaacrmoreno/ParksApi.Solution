e1d4316dd269181033b6b6aa75cd929663adc0ba
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-11 17:03:21 -0700

    :rocket: HTo the moon

--- ParkApi/Startup.cs
+++ ParkApi/Startup.cs
@@ -4,7 +4,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ParkApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 namespace ParkApi
 {
@@ -23,6 +27,27 @@ namespace ParkApi
             services.AddDbContext<ParkApiContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Isaac Moreno",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/isaacrmoreno"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -31,6 +56,17 @@ namespace ParkApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            c.RoutePrefix = string.Empty;
+            });
 
             // app.UseHttpsRedirection();
 
